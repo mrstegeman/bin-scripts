99ea9568f0ac8182a27905aa6dda96e45a7e1012
Mike Stegeman <mrstegeman@gmail.com>
2011-05-20 11:38:46 -0400

    prettyxml actually works with filenames now

--- prettyxml.pl
+++ prettyxml.pl
@@ -6,7 +6,15 @@ use XML::Parser;
 my $xml = XML::Twig->new(pretty_print => 'indented');
 
 if ($ARGV[0]) {
-    $xml->parse($ARGV[0]);
+    if (-r $ARGV[0]) {
+        open(F, "<$ARGV[0]");
+        $xml->parse(\*F);
+        close(F);
+    }
+    else {
+        print("invalid filename\n");
+        exit(1);
+    }
 } else {
     $xml->parse(\*STDIN);
 }
