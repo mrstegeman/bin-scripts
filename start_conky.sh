4433875545462f57311ad2131a0f351e7622c82c
Mike Stegeman <mrstegeman@gmail.com>
2011-06-26 13:09:59 -0400

    add some sleeps to conky startup for taskwarrior

--- start_conky.sh
+++ start_conky.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-conky -c /home/michael/.conkyrc_rings &
-conky -c /home/michael/.conkyrc_bills &
-conky -c /home/michael/.conkyrc_tv &
-conky -c /home/michael/.conkyrc_clock &
-conky -c /home/michael/.conkyrc_todo &
+conky -c /home/michael/.config/conky/.conkyrc_clock &
+conky -c /home/michael/.config/conky/.conkyrc_rings &
+conky -c /home/michael/.config/conky/.conkyrc_bills &
+sleep 1
+conky -c /home/michael/.config/conky/.conkyrc_tv &
+sleep 1
+conky -c /home/michael/.config/conky/.conkyrc_todo &
